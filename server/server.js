00c192e63da10fe5f33afb58d4a4d5caedabafaa
karam122 <karam.pucit@gmail.com>
2024-04-15 17:37:43 +0500

    api route + DB settings

--- server/server.js
+++ server/server.js
@@ -1,8 +1,19 @@
-import express from 'express';
+import express, { json } from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config';
 
 const server = express();
 
+server.use(express.json());
 let PORT = 8000;
+
+mongoose.connect(process.env.DB_LOCATION, {
+  autoIndex: true
+});
+
+server.post('/signup', (req, res) => {
+  res.json(req.body);
+});
 
 server.listen(PORT, () => {
   console.log(`Server iSs Running on PORT ${PORT}`);
